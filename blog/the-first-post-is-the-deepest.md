495e0af29fca2f753d19cc700ccc8db8afd66564
mrmodise <modisemorebodi@gmail.com>
2020-07-16 18:51:32 +0200

    feat: add Scully blog

---
+++ blog/the-first-post-is-the-deepest.md
@@ -0,0 +1,7 @@
+---
+title: The First Post is the Deepest
+description: blog description
+published: false
+---
+
+# The First Post is the Deepest
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
 
-const routes: Routes = [{ path: '', component: HomeComponent}];
+const routes: Routes = [{ path: '', component: HomeComponent}, { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
